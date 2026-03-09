236f51539a58861556c0562417e3aec66020bb34
GkVasilis <cs05152@uoi.gr>
2026-03-09 17:29:47 +0200

    Initial project upload

---
+++ db3_5030_5152/db3_5030_5152/src/main/java/mye30/project/db3_5030_5152/Db350305152Application.java
@@ -0,0 +1,13 @@
+package mye30.project.db3_5030_5152;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Db350305152Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Db350305152Application.class, args);
+	}
+
+}
---
+++ db3_5030_5152/db3_5030_5152/src/test/java/mye30/project/db3_5030_5152/Db350305152ApplicationTests.java
@@ -0,0 +1,13 @@
+package mye30.project.db3_5030_5152;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Db350305152ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
